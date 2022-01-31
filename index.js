306548789b8286d48dd92b6f21cac8724aba5ab1
wagnevic <victoria.wagner@brgenns.ac.at>
2022-01-31 16:43:58 +0100

    3 files modified

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ const server = require ('http').createServer(app)
 
 const port = 3000
 
+const axios = require('axios')
+const pokeapi = 'https://pokeapi.co/api/v2/'
+
 server.listen(port, function() {
 
   console.log('Websaver läuf. Pott: %d', port)
 })
 
+app.get('/', (req, res) => {
+//console.log(req);
+//res.send('hällo ägän (not)')
+axios
+  .get(pokeapi + 'pokemon/ditto')
+  .then(response =>{
+     res.send
+     ('<img src= " ' +
+  response.data.sprites.other['official-artwork'].front_default +
+  '">'
+     );
+}).catch(error => {
+  console.log(error)
+  res.send(error)
+})
+});
